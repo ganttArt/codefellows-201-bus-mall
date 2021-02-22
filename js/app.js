dcbd3797bf1d6019ac589133551de5e7ef7eced1
Chris Gantt <gantt.art@gmail.com>
2021-02-22 13:33:37 -0800

    Created Product construtor and instantiated all products with pictures in assets folder.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
 'use strict';
+
+let totalClicks = 0;
+
+function Product(name, filepath, timesShown) {
+  this.name = name;
+  this.filepath = filepath;
+  this.timesShown = timesShown;
+  this.timesClicked = 0;
+
+  Product.all.push(this);
+}
+
+Product.all = [];
+
+new Product('Bag', 'assets/bag.jpg', 0);
+new Product('Banana', 'assets/banana.jpg', 0);
+new Product('Bathroom', 'assets/bathroom.jpg', 0);
+new Product('Boots', 'assets/boots.jpg', 0);
+new Product('Breakfast', 'assets/breakfast.jpg', 0);
+new Product('Bubblegum', 'assets/bubblegum.jpg', 0);
+new Product('Chair', 'assets/chair.jpg', 0);
+new Product('Cthulhu', 'assets/cthulhu.jpg', 0);
+new Product('Dog Duck', 'assets/dog-duck.jpg', 0);
+new Product('Dragon', 'assets/dragon.jpg', 0);
+new Product('Pen', 'assets/pen.jpg', 0);
+new Product('Pet Sweep', 'assets/pet-sweep.jpg', 0);
+new Product('Scissors', 'assets/scissors.jpg', 0);
+new Product('Shark', 'assets/shark.jpg', 0);
+new Product('Sweep', 'assets/sweep.png', 0);
+new Product('Tauntaun', 'assets/tauntaun.jpg', 0);
+new Product('Unicorn', 'assets/unicorn.jpg', 0);
+new Product('USB', 'assets/usb.gif', 0);
+new Product('Water Can', 'assets/water-can.jpg', 0);
+new Product('Wine Glass', 'assets/wine-glass.jpg', 0);
